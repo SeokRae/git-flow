d604395fe99f3a3ceb50c5f9909281036651d685
seokrae <kslbsh@gmail.com>
2021-09-07 23:56:42 +0900

    Initial commit

---
+++ src/test/java/com/example/gitflow/GitFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
